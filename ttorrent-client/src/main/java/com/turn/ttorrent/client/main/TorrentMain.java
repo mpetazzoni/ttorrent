78b94a0000606bf740f2ada31ba1b56e9d3726af
Shevek <shevek@anarres.org>
2014-10-10 17:02:30 -0700

    Build useful application packages.

--- ttorrent-client/src/main/java/com/turn/ttorrent/client/main/TorrentMain.java
+++ ttorrent-client/src/main/java/com/turn/ttorrent/client/main/TorrentMain.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.turn.ttorrent.cli;
+package com.turn.ttorrent.client.main;
 
 import com.turn.ttorrent.protocol.torrent.Torrent;
 import com.turn.ttorrent.protocol.torrent.TorrentCreator;
