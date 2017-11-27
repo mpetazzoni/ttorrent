77da48334bd1d99607cb531874c0de8cfd3979e0
Maxim Zaytsev <Maxim.Zaytsev@jetbrains.com>
2017-11-27 17:10:04 +0300

    fixed incorrect writing data to socket channel

--- src/main/java/com/turn/ttorrent/client/network/keyProcessors/WritableKeyProcessor.java
+++ src/main/java/com/turn/ttorrent/client/network/keyProcessors/WritableKeyProcessor.java
@@ -1,12 +1,12 @@
 package com.turn.ttorrent.client.network.keyProcessors;
 
-import com.turn.ttorrent.client.network.ConnectionListener;
 import com.turn.ttorrent.client.network.KeyAttachment;
 import com.turn.ttorrent.client.network.WriteTask;
 import com.turn.ttorrent.common.LoggerUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
@@ -15,7 +15,6 @@ import java.nio.channels.SocketChannel;
 public class WritableKeyProcessor implements KeyProcessor {
 
   private static final Logger logger = LoggerFactory.getLogger(WritableKeyProcessor.class);
-  private WriteTask processedTask = null;
 
   @Override
   public void process(SelectionKey key) throws IOException {
@@ -28,39 +27,38 @@ public class WritableKeyProcessor implements KeyProcessor {
 
     SocketChannel socketChannel = (SocketChannel) channel;
 
-    if (mustGetNextTaskFromQueue()) {
-      if (currentTaskExistAndDone()) {
-        processedTask.getListener().onWriteDone();
-      }
-      Object attachment = key.attachment();
-      if (!(attachment instanceof KeyAttachment)) {
-        logger.warn("incorrect instance of attachment for channel {}", new Object[]{socketChannel.socket()});
-        processedTask.getListener().onWriteFailed();
-        processedTask = null;
-        key.cancel();
-        return;
-      }
-      processedTask = ((KeyAttachment) attachment).getWriteTasks().poll();
-      if (processedTask == null) {
-        key.interestOps(SelectionKey.OP_READ);
-        return;
-      }
+    Object attachment = key.attachment();
+    if (!(attachment instanceof KeyAttachment)) {
+      logger.error("incorrect instance of attachment for channel {}", channel);
+      key.cancel();
+      return;
     }
+
+    KeyAttachment keyAttachment = (KeyAttachment) attachment;
+
+    if (keyAttachment.getWriteTasks().isEmpty()) {
+      key.interestOps(SelectionKey.OP_READ);
+      return;
+    }
+
+    WriteTask processedTask = keyAttachment.getWriteTasks().peek();
+
+    if (!processedTask.getByteBuffer().hasRemaining()) {
+      keyAttachment.getWriteTasks().remove();
+      return;
+    }
+
     try {
-      socketChannel.write(processedTask.getByteBuffer());
+      int writeCount = socketChannel.write(processedTask.getByteBuffer());
+      if (writeCount < 0) {
+        throw new EOFException("Reached end of stream while writing");
+      }
     } catch (IOException e) {
       LoggerUtils.errorAndDebugDetails(logger, "unable to write data to channel {}", socketChannel, e);
       processedTask.getListener().onWriteFailed();
-      processedTask = null;
+      keyAttachment.getWriteTasks().clear();
+      key.cancel();
     }
-  }
-
-  private boolean currentTaskExistAndDone() {
-    return processedTask != null && !processedTask.getByteBuffer().hasRemaining();
-  }
-
-  private boolean mustGetNextTaskFromQueue() {
-    return processedTask == null || !processedTask.getByteBuffer().hasRemaining();
   }
 
   @Override
