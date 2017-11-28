a90d09d0ffb6d003cf7ef154b4cb5f532304d600
Maxim Zaytsev <Maxim.Zaytsev@jetbrains.com>
2017-11-28 11:18:36 +0300

    removed unused import

--- src/main/java/com/turn/ttorrent/client/network/ConnectionManager.java
+++ src/main/java/com/turn/ttorrent/client/network/ConnectionManager.java
@@ -10,7 +10,6 @@ import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
