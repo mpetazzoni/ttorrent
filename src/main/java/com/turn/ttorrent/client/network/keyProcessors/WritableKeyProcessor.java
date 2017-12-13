20bd92d8c31d53c0876f93ccf748d7d0234a0def
Maxim Zaytsev <Maxim.Zaytsev@jetbrains.com>
2017-12-13 11:57:56 +0300

    changed log message on write failed

--- src/main/java/com/turn/ttorrent/client/network/keyProcessors/WritableKeyProcessor.java
+++ src/main/java/com/turn/ttorrent/client/network/keyProcessors/WritableKeyProcessor.java
@@ -55,8 +55,7 @@ public class WritableKeyProcessor implements KeyProcessor {
       }
 
     } catch (IOException e) {
-      LoggerUtils.errorAndDebugDetails(logger, "unable to write data to channel {}", socketChannel, e);
-      processedTask.getListener().onWriteFailed("I/O error occurs. " + e.getMessage(), e);
+      processedTask.getListener().onWriteFailed("I/O error occurs on write to channel " + socketChannel, e);
       keyAttachment.getWriteTasks().clear();
       key.cancel();
     }
