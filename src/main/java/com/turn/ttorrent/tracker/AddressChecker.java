60a45747c98e49711782691c41d67ddfffdc1de8
Maxim Zaytsev <Maxim.Zaytsev@jetbrains.com>
2018-01-11 17:44:05 +0300

    changed incorrect comment

--- src/main/java/com/turn/ttorrent/tracker/AddressChecker.java
+++ src/main/java/com/turn/ttorrent/tracker/AddressChecker.java
@@ -3,7 +3,7 @@ package com.turn.ttorrent.tracker;
 public interface AddressChecker {
 
   /**
-   * this method must return true if is correct ip and other peers can connect to this peer. If this method return false
+   * this method must return true if is incorrect ip and other peers can not connect to this peer. If this method return true
    * tracker doesn't register the peer for current torrent
    * @param ip specified address
    */
