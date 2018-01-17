8ed2168a6e932d4513256762d83ea05d483dd509
Maxim Zaytsev <Maxim.Zaytsev@jetbrains.com>
2018-01-17 15:07:56 +0300

    just replaced printstacktrace by correct logging

--- src/main/java/com/turn/ttorrent/client/peer/SharingPeer.java
+++ src/main/java/com/turn/ttorrent/client/peer/SharingPeer.java
@@ -20,6 +20,7 @@ import com.turn.ttorrent.client.SharedTorrent;
 import com.turn.ttorrent.client.network.ConnectionManager;
 import com.turn.ttorrent.client.network.WriteListener;
 import com.turn.ttorrent.client.network.WriteTask;
+import com.turn.ttorrent.common.LoggerUtils;
 import com.turn.ttorrent.common.Peer;
 import com.turn.ttorrent.common.TorrentHash;
 import com.turn.ttorrent.common.protocol.PeerMessage;
@@ -329,7 +330,7 @@ public class SharingPeer extends Peer implements MessageListener, SharingPeerInf
           connectionManager.closeChannel(socketChannel);
         }
       } catch (IOException e) {
-        e.printStackTrace();
+        LoggerUtils.errorAndDebugDetails(logger, "cannot close socket channel. Peer {}", this, e);
       }
     }
 
