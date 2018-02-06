9da2a84f73c313d4a12f41034316e45d3948255c
Maxim Zaytsev <Maxim.Zaytsev@jetbrains.com>
2018-02-06 13:46:06 +0300

    TW-35395 Artifacts of old builds cannot be downloaded via torrent (limit on the number of seeded artifacts) Added interface for announceable torrent. Added implementation for the interface. Added class for calculation torrent statistic (downloaded, uploaded, left count)

---
+++ src/main/java/com/turn/ttorrent/common/AnnounceableTorrent.java
@@ -0,0 +1,28 @@
+package com.turn.ttorrent.common;
+
+import java.util.List;
+
+public interface AnnounceableTorrent extends TorrentHash {
+
+  /*
+  * Number of bytes uploaded by the client for this torrent
+  * */
+  long getUploaded();
+
+  /*
+  * Number of bytes downloaded by the client for this torrent
+  * */
+  long getDownloaded();
+
+  /*
+  * Number of bytes left to download by the client for this torrent
+  * */
+  long getLeft();
+
+  /**
+   * @return all tracker for announce
+   * @see <a href="http://bittorrent.org/beps/bep_0012.html"></a>
+   */
+  List<List<String>> getAnnounceUrls();
+
+}
---
+++ src/main/java/com/turn/ttorrent/common/AnnounceableTorrentImpl.java
@@ -0,0 +1,52 @@
+package com.turn.ttorrent.common;
+
+import java.util.Collections;
+import java.util.List;
+
+public class AnnounceableTorrentImpl implements AnnounceableTorrent {
+
+  private final TorrentStatistic myTorrentStatistic;
+  private final String myHexInfoHash;
+  private final byte[] myInfoHash;
+  private final List<List<String>> myAnnounceUrls;
+
+  public AnnounceableTorrentImpl(TorrentStatistic torrentStatistic,
+                                 String hexInfoHash,
+                                 byte[] infoHash,
+                                 List<List<String>> announceUrls) {
+    myTorrentStatistic = torrentStatistic;
+    this.myHexInfoHash = hexInfoHash;
+    this.myInfoHash = infoHash;
+    this.myAnnounceUrls = Collections.unmodifiableList(announceUrls);
+  }
+
+  @Override
+  public long getUploaded() {
+    return myTorrentStatistic.getUploadedBytes();
+  }
+
+  @Override
+  public long getDownloaded() {
+    return myTorrentStatistic.getDownloadedBytes();
+  }
+
+  @Override
+  public long getLeft() {
+    return myTorrentStatistic.getLeftBytes();
+  }
+
+  @Override
+  public List<List<String>> getAnnounceUrls() {
+    return myAnnounceUrls;
+  }
+
+  @Override
+  public byte[] getInfoHash() {
+    return myInfoHash;
+  }
+
+  @Override
+  public String getHexInfoHash() {
+    return myHexInfoHash;
+  }
+}
