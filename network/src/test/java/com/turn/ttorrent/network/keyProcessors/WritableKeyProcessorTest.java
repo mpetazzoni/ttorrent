cc66628fb6a1450c197a79f3edb5b863d9d076d7
Maxim Zaytsev <Maxim.Zaytsev@jetbrains.com>
2018-02-22 11:33:01 +0300

    added some tests for writable key processor

--- network/src/test/java/com/turn/ttorrent/network/keyProcessors/WritableKeyProcessorTest.java
+++ network/src/test/java/com/turn/ttorrent/network/keyProcessors/WritableKeyProcessorTest.java
@@ -3,9 +3,12 @@ package com.turn.ttorrent.network.keyProcessors;
 import com.turn.ttorrent.network.WriteAttachment;
 import com.turn.ttorrent.network.WriteListener;
 import com.turn.ttorrent.network.WriteTask;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
@@ -17,31 +20,83 @@ import static org.mockito.Mockito.*;
 public class WritableKeyProcessorTest {
 
   private SelectionKey myKey;
+  private SocketChannel myChannel;
+  private WritableKeyProcessor myWritableKeyProcessor;
+  private WriteAttachment myWriteAttachment;
+  private BlockingQueue<WriteTask> myQueue;
 
 
+  @SuppressWarnings("unchecked")
   @BeforeMethod
   public void setUp() throws Exception {
     myKey = mock(SelectionKey.class);
+    myChannel = mock(SocketChannel.class);
+    myWritableKeyProcessor = new WritableKeyProcessor();
+    when(myKey.channel()).thenReturn(myChannel);
+    when(myKey.interestOps()).thenReturn(SelectionKey.OP_WRITE);
+    myWriteAttachment = mock(WriteAttachment.class);
+    myQueue = mock(BlockingQueue.class);
   }
 
   public void testThatOnWriteDoneInvoked() throws Exception {
-    WritableKeyProcessor writableKeyProcessor = new WritableKeyProcessor();
+    final ByteBuffer data = ByteBuffer.allocate(10);
 
-    SocketChannel channel = mock(SocketChannel.class);
-    WriteAttachment writeAttachment = mock(WriteAttachment.class);
-    BlockingQueue<WriteTask> queue = mock(BlockingQueue.class);
+    //imitate writing byte buffer
+    when(myChannel.write(eq(data))).then(new Answer<Integer>() {
+      @Override
+      public Integer answer(InvocationOnMock invocationOnMock) throws Throwable {
+        data.position(data.capacity());
+        return data.capacity();
+      }
+    });
+
     WriteListener listener = mock(WriteListener.class);
 
-    when(myKey.channel()).thenReturn(channel);
-    when(myKey.interestOps()).thenReturn(SelectionKey.OP_WRITE);
-    when(queue.peek()).thenReturn(new WriteTask(channel, ByteBuffer.allocate(0), listener));
-    when(writeAttachment.getWriteTasks()).thenReturn(queue);
+    when(myQueue.peek()).thenReturn(new WriteTask(myChannel, data, listener));
+    when(myWriteAttachment.getWriteTasks()).thenReturn(myQueue);
 
-    myKey.attach(writeAttachment);
+    myKey.attach(myWriteAttachment);
 
-    writableKeyProcessor.process(myKey);
+    myWritableKeyProcessor.process(myKey);
 
     verify(listener).onWriteDone();
   }
 
+  public void testThatOnWriteFailedInvokedIfChannelThrowException() throws Exception {
+    when(myChannel.write(any(ByteBuffer.class))).thenThrow(new IOException());
+
+    WriteListener listener = mock(WriteListener.class);
+
+    when(myQueue.peek()).thenReturn(new WriteTask(myChannel, ByteBuffer.allocate(1), listener));
+    when(myWriteAttachment.getWriteTasks()).thenReturn(myQueue);
+    myKey.attach(myWriteAttachment);
+
+    myWritableKeyProcessor.process(myKey);
+
+    verify(listener).onWriteFailed(anyString(), any(Throwable.class));
+  }
+
+  public void checkThatWriteTaskDoesntRemovedIfBufferIsNotWrittenInOneStep() throws Exception {
+    final ByteBuffer data = ByteBuffer.allocate(10);
+
+    //imitate writing only one byte of byte buffer
+    when(myChannel.write(eq(data))).then(new Answer<Integer>() {
+      @Override
+      public Integer answer(InvocationOnMock invocationOnMock) throws Throwable {
+        data.position(data.capacity() - 1);
+        return data.position();
+      }
+    });
+
+    WriteListener listener = mock(WriteListener.class);
+
+    when(myQueue.peek()).thenReturn(new WriteTask(myChannel, data, listener));
+    when(myWriteAttachment.getWriteTasks()).thenReturn(myQueue);
+
+    myKey.attach(myWriteAttachment);
+
+    myWritableKeyProcessor.process(myKey);
+
+    verify(listener, never()).onWriteDone();
+  }
 }
