da096649d3b98cc68b6cae8a25357e9bd06d7e60
Maxim Zaytsev <Maxim.Zaytsev@jetbrains.com>
2018-02-26 11:44:02 +0300

    added javadoc for FileTorrent interface

--- common/src/main/java/com/turn/ttorrent/common/FileTorrent.java
+++ common/src/main/java/com/turn/ttorrent/common/FileTorrent.java
@@ -2,8 +2,14 @@ package com.turn.ttorrent.common;
 
 public interface FileTorrent {
 
+  /**
+   * @return path to torrent root directory which contains files from torrent and is used for files downloading
+   */
   String getDownloadDirPath();
 
+  /**
+   * @return path to .torrent file
+   */
   String getDotTorrentFilePath();
 
 }
