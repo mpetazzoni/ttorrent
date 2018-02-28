8d4fc2a8f72b9085b1f095e8a1ca70fea712ea7b
Maxim Zaytsev <Maxim.Zaytsev@jetbrains.com>
2018-02-28 12:30:26 +0300

    fixed incorrect order of params in constructor invocation

--- common/src/main/java/com/turn/ttorrent/common/protocol/http/HTTPAnnounceRequestMessage.java
+++ common/src/main/java/com/turn/ttorrent/common/protocol/http/HTTPAnnounceRequestMessage.java
@@ -263,7 +263,7 @@ public class HTTPAnnounceRequestMessage extends HTTPTrackerMessage
 
       return new HTTPAnnounceRequestMessage(Constants.EMPTY_BUFFER, infoHash,
               new Peer(ip, port, ByteBuffer.wrap(peerId)),
-              downloaded, uploaded, left, compact, noPeerId,
+              uploaded, downloaded, left, compact, noPeerId,
               event, numWant);
     } catch (InvalidBEncodingException ibee) {
       throw new MessageValidationException(
