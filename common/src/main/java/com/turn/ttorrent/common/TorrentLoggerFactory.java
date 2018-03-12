878d23c91026e06c53ffa7648dbf9432c58ae8ff
Maxim Zaytsev <Maxim.Zaytsev@jetbrains.com>
2018-03-12 16:20:55 +0300

    added torrent logger factory which just creates loggers with hardcoded category

---
+++ common/src/main/java/com/turn/ttorrent/common/TorrentLoggerFactory.java
@@ -0,0 +1,11 @@
+package com.turn.ttorrent.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class TorrentLoggerFactory {
+
+  public static Logger getLogger() {
+    return LoggerFactory.getLogger("jetbrains.TorrentLibrary");
+  }
+}
