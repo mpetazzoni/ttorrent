8b01edeb1b0cef413ff882302150e4ed762919b9
Maxim.Zaytsev <Maxim.Zaytsev@jetbrains.com>
2018-07-03 13:36:21 +0300

    javadoc fixes

--- common/src/main/java/com/turn/ttorrent/common/AnnounceableTorrent.java
+++ common/src/main/java/com/turn/ttorrent/common/AnnounceableTorrent.java
@@ -4,19 +4,19 @@ import java.util.List;
 
 public interface AnnounceableTorrent extends TorrentHash {
 
-  /*
-  * Number of bytes uploaded by the client for this torrent
-  * */
+  /**
+   * @return number of bytes uploaded by the client for this torrent
+   */
   long getUploaded();
 
-  /*
-  * Number of bytes downloaded by the client for this torrent
-  * */
+  /**
+   * @return number of bytes downloaded by the client for this torrent
+   */
   long getDownloaded();
 
-  /*
-  * Number of bytes left to download by the client for this torrent
-  * */
+  /**
+   * @return number of bytes left to download by the client for this torrent
+   */
   long getLeft();
 
   /**
