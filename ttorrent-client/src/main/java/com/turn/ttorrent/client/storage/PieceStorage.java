3153178653ba443f8a2cd1a6fff3b3bd5c433876
Maxim.Zaytsev <Maxim.Zaytsev@jetbrains.com>
2018-07-06 13:06:50 +0300

    fixed compile error

---
+++ ttorrent-client/src/main/java/com/turn/ttorrent/client/storage/PieceStorage.java
@@ -0,0 +1,17 @@
+package com.turn.ttorrent.client.storage;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.BitSet;
+
+public interface PieceStorage extends Closeable {
+
+  void savePiece(int pieceIndex, byte[] pieceData) throws IOException;
+
+  byte[] readPiecePart(int pieceIndex, int offset, int length) throws IOException;
+
+  BitSet getAvailablePieces();
+
+  void delete() throws IOException;
+
+}
